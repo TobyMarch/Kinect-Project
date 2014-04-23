79ae12eec5a6904151b59fe59b021521857f49d8
TobyMarch <tmarch1@ithaca.edu>
2014-04-23 10:49:12 -0400

    Fixed CoinPrefab

--- Unity Research Game/Assets/Scripts/CoinScript.cs
+++ Unity Research Game/Assets/Scripts/CoinScript.cs
@@ -8,13 +8,10 @@ public class CoinScript : MonoBehaviour {
 	#endregion
 	
 	public float rotationAmount = 2.0f;
-	MeshRenderer myRenderer;
 	private GameObject playerCharacter;
 	
 	
 	void vanish () {
-		//GetComponent<MeshRenderer>().enabled = false;
-		myRenderer.enabled = false;
 		Destroy(gameObject);
 		playerCharacter.GetComponent<BDGameScript>().coinCollected();
 	}
@@ -25,9 +22,7 @@ public class CoinScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		myRenderer = GetComponent<MeshRenderer>();
 		playerCharacter = GameObject.Find(PlayerCharacterName);
-		//transform.Rotate(0,0,0);
 	}
 	
 	// Update is called once per frame
